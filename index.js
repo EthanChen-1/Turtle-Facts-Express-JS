d8568ba118cfb156e855642f0c6bb976839ed97a
Ethan Chen <70767744+EthanChen-1@users.noreply.github.com>
2021-06-18 17:28:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,22 +27,55 @@ app.get('/css/:file', function(req, res){
 });
 
 app.get('/statistics', function(req, res){
-    let get_info_db = `SELECT * from page_statistics`;
+    console.log( req.socket.remoteAddress + " went to " + req.query);
+    let get_info_db = `SELECT page_name, page_views from page_statistics`;
     turtle_db_con.query(get_info_db, function(err, result){
         if(err) throw err;
         console.log(result);
-        let body = "";
+        let table_headers = ``;
+        let table_values = ``;
         for(let i = 0; i < result.length; i++){
-            body += `PAGE: ${result[i].page_name} | VIEWS: ${result[i].page_views} <br/>`;
+            table_headers += `<th>${result[i].page_name}</th>`;
+            table_values += `<td>${result[i].page_views}</td>`;
         }
-        res.send(`<h1> ${body} </h1> <p><a href="/">Back to Search</a></p>`);
+        console.log(table_headers);
+        console.log(table_values);
+        res.send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Aquatic Turtle Page Statistics</title>
+                <link rel="stylesheet" href="/css/style-stats.css">
+            </head>
+            <body>
+                <table>
+                    <tr>
+                        <td>Page Name</td>
+                        ${table_headers}
+                    </tr>
+                    <tr>
+                        <td>Page Views</td>
+                        ${table_values}
+                    </tr>
+                </table> <br>
+                <div id="footer">
+                    <form id='back-to-search' action="/" method="get">
+                        <input type="submit" value="Back to Search">
+                    </form>
+                </div>
+            </body>
+            </html>
+        `);
     });
 });
 
 //Creates connection to database
-const sql = require('mysql');
+const mysql = require('mysql');
 let {host, user, password, database} = require("./auth/auth.json");
-const turtle_db_con = sql.createConnection({
+const turtle_db_con = mysql.createConnection({
     host: `${host}`,
     user: `${user}`,
     password: `${password}`,
@@ -54,6 +87,12 @@ turtle_db_con.connect(function(err){
         return console.error('error: ' + err.message);
     }
     console.log(`Connected to the MySQL server on ${database}`);
-});
+})
 
+// turtle_db_con.end(function(err){
+//     if(err){
+//         return console.error('error: ' + err.message);
+//     }
+//     console.log('Closed the database connection.');
+// })
 app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
